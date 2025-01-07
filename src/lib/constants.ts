bcd558c6bc22ffdff22b432b2fd6008c3317321a
Movin21 <movinliyanage@gmail.com>
2025-01-07 22:28:20 +0530

    Preveiw Image added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
-  "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
+  "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/306269821_434117868824643_8641128237022805677_n.png?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=I7ajw6m4LtkQ7kNvgG6H7Uu&_nc_oc=AdgcqXUaxc5QLVEgm59grbNL7HP-PkQAIfI00Ns5URL9xcUh_Lrq_u44xOYUcO-06e8fXk6IPqUu0QHAEOlMMH59&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=Ar9fYo6UoW3Mr21agHxrmMV&oh=00_AYA07-v7Mg00ZMY2LbFOomqst7nnCuihrCmLY9RIFKX0Uw&oe=67831BE5";
